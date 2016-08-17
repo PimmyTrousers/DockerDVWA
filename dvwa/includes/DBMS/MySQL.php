eb521d9fec92c452c0ab4667c11f444dd29e4d8a
Pim Trouerbach <pim.trouerbach1@gmail.com>
2016-08-17 10:51:06 -0700

    Somewhat reverting

--- dvwa/includes/DBMS/MySQL.php
+++ dvwa/includes/DBMS/MySQL.php
@@ -11,81 +11,66 @@ $_DVWA[ 'db_database' ] = 'dvwa';
 $_DVWA[ 'db_user' ] = 'root';
 $_DVWA[ 'db_password' ] = 'p@ssw0rd';
 
-if( !@mysql_connect( $_DVWA[ 'db_server' ],  $_DVWA[ 'db_user' ],  $_DVWA[ 'db_password' ] ) ) {
-	//dvwaMessagePush( "Could not connect to the database - please check the config file." );
+if( !@($GLOBALS["___mysqli_ston"] = mysqli_connect( $_DVWA[ 'db_server' ],  $_DVWA[ 'db_user' ],  $_DVWA[ 'db_password' ] )) ) {
+	//dvwaMessagePush( "Could not connect to the MySQL service.<br />Please check the config file." );
 	//dvwaPageReload();
 }
-
 // Create database
-$drop_db = "DROP DATABASE IF EXISTS " . $_DVWA[ 'db_database' ] . ";";
-if( !@mysql_query ( $drop_db ) ) {
-	//dvwaMessagePush( "Could not drop existing database<br />SQL: ".mysql_error() );
+$drop_db = "DROP DATABASE IF EXISTS {$_DVWA[ 'db_database' ]};";
+if( !@mysqli_query($GLOBALS["___mysqli_ston"],  $drop_db ) ) {
+	//dvwaMessagePush( "Could not drop existing database<br />SQL: " . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) );
 	//dvwaPageReload();
 }
-
-$create_db = "CREATE DATABASE " . "dvwa" . ";";
-
-if( !@mysql_query ( "CREATE DATABASE" . $create_db . ";" ) ) {
-	//dvwaMessagePush( "Could not create database<br />SQL: ".mysql_error() );
+$create_db = "CREATE DATABASE {$_DVWA[ 'db_database' ]};";
+if( !@mysqli_query($GLOBALS["___mysqli_ston"],  $create_db ) ) {
+	//dvwaMessagePush( "Could not create database<br />SQL: " . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) );
 	//dvwaPageReload();
 }
-
-//dvwaMessagePush( "Database has been created." );
-
+dvwaMessagePush( "Database has been created." );
 // Create table 'users'
-if( !@mysql_select_db( $_DVWA[ 'db_database' ] ) ) {
+if( !@((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE " . $_DVWA[ 'db_database' ])) ) {
 	//dvwaMessagePush( 'Could not connect to database.' );
 	//dvwaPageReload();
 }
-
-$create_tb = "CREATE TABLE users (user_id int(6),first_name varchar(15),last_name varchar(15), user varchar(15), password varchar(32),avatar varchar(70), PRIMARY KEY (user_id));";
-if( !mysql_query( "CREATE DATABASE dvwa;" ) ){
-	//dvwaMessagePush( "Table could not be created<br />SQL: ".mysql_error() );
+$create_tb = "CREATE TABLE users (user_id int(6),first_name varchar(15),last_name varchar(15), user varchar(15), password varchar(32),avatar varchar(70), last_login TIMESTAMP, failed_login INT(3), PRIMARY KEY (user_id));";
+if( !mysqli_query($GLOBALS["___mysqli_ston"],  $create_tb ) ) {
+	//dvwaMessagePush( "Table could not be created<br />SQL: " . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) );
 	//dvwaPageReload();
 }
-
 //dvwaMessagePush( "'users' table was created." );
-
 // Insert some data into users
-
 // Get the base directory for the avatar media...
-$baseUrl = 'http://'.$_SERVER[ 'SERVER_NAME' ].$_SERVER[ 'PHP_SELF' ];
-$stripPos = strpos( $baseUrl, 'dvwa/setup.php' );
-$baseUrl = substr( $baseUrl, 0, $stripPos ).'dvwa/hackable/users/';
-
+$baseUrl  = 'http://' . $_SERVER[ 'SERVER_NAME' ] . $_SERVER[ 'PHP_SELF' ];
+$stripPos = strpos( $baseUrl, 'setup.php' );
+$baseUrl  = substr( $baseUrl, 0, $stripPos ) . 'hackable/users/';
 $insert = "INSERT INTO users VALUES
-	('1','admin','admin','admin',MD5('password'),'{$baseUrl}admin.jpg'),
-	('2','Gordon','Brown','gordonb',MD5('abc123'),'{$baseUrl}gordonb.jpg'),
-	('3','Hack','Me','1337',MD5('charley'),'{$baseUrl}1337.jpg'),
-	('4','Pablo','Picasso','pablo',MD5('letmein'),'{$baseUrl}pablo.jpg'),
-	('5','Bob','Smith','smithy',MD5('password'),'{$baseUrl}smithy.jpg');";
-if( !mysql_query( $insert ) ){
-	//dvwaMessagePush( "Data could not be inserted into 'users' table<br />SQL: ".mysql_error() );
+	('1','admin','admin','admin',MD5('password'),'{$baseUrl}admin.jpg', NOW(), '0'),
+	('2','Gordon','Brown','gordonb',MD5('abc123'),'{$baseUrl}gordonb.jpg', NOW(), '0'),
+	('3','Hack','Me','1337',MD5('charley'),'{$baseUrl}1337.jpg', NOW(), '0'),
+	('4','Pablo','Picasso','pablo',MD5('letmein'),'{$baseUrl}pablo.jpg', NOW(), '0'),
+	('5','Bob','Smith','smithy',MD5('password'),'{$baseUrl}smithy.jpg', NOW(), '0');";
+if( !mysqli_query($GLOBALS["___mysqli_ston"],  $insert ) ) {
+	//dvwaMessagePush( "Data could not be inserted into 'users' table<br />SQL: " . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) );
 	//dvwaPageReload();
 }
 //dvwaMessagePush( "Data inserted into 'users' table." );
-
 // Create guestbook table
 $create_tb_guestbook = "CREATE TABLE guestbook (comment_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, comment varchar(300), name varchar(100), PRIMARY KEY (comment_id));";
-
-if( !mysql_query( $create_tb_guestbook ) ){
-	//dvwaMessagePush( "Table could not be created<br />SQL: ".mysql_error() );
+if( !mysqli_query($GLOBALS["___mysqli_ston"],  $create_tb_guestbook ) ) {
+	//dvwaMessagePush( "Table could not be created<br />SQL: " . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) );
 	//dvwaPageReload();
 }
-
 //dvwaMessagePush( "'guestbook' table was created." );
-
 // Insert data into 'guestbook'
-//$insert = "INSERT INTO guestbook VALUES
-//('1','This is a test comment.','test');";
-
-if( !mysql_query( "INSERT INTO guestbook VALUES ('1','This is a test comment.','test');" ) ){
-	//dvwaMessagePush( "Data could not be inserted into 'guestbook' table<br />SQL: ".mysql_error() );
-	//dvwaPageReload();
+$insert = "INSERT INTO guestbook VALUES ('1','This is a test comment.','test');";
+if( !mysqli_query($GLOBALS["___mysqli_ston"],  $insert ) ) {
+	//dvwaMessagePush( "Data could not be inserted into 'guestbook' table<br />SQL: " . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) );
+//	dvwaPageReload();
 }
 //dvwaMessagePush( "Data inserted into 'guestbook' table." );
-
-//dvwaMessagePush( "Setup successful!" );
+// Done
+//dvwaMessagePush( "<em>Setup successful</em>!" );
+if( !dvwaIsLoggedIn())
+	//dvwaMessagePush( "Please <a href='login.php'>login</a>.<script>setTimeout(function(){window.location.href='login.php'},5000);</script>" );
 //dvwaPageReload();
-
 ?>
